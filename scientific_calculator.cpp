9f6c2dcde89ca1a6c6018bdb3f95dea3ab806cef
SamraAzizi <fahimaazizi369@gmail.com>
2024-03-15 20:26:59 +0500

    my commit

--- scientific_calculator.cpp
+++ scientific_calculator.cpp
@@ -5,3 +5,20 @@
 #include <QPushButton>
 #include <cmath>
 
+class Calculator : public QWidget {
+public:
+    Calculator(QWidget *parent = nullptr) : QWidget(parent) {
+        // Set up the layout
+        QGridLayout *layout = new QGridLayout(this);
+        QLineEdit *display = new QLineEdit();
+        layout->addWidget(display, 0, 0, 1, 5);
+
+        // Create buttons for numbers and operations
+        const QString buttons[5][5] = {
+            {"7", "8", "9", "/", "sqrt"},
+            {"4", "5", "6", "*", "pow"},
+            {"1", "2", "3", "-", "sin"},
+            {"0", ".", "=", "+", "cos"},
+            {"Clear", "Clear All", "Backspace", "(", ")"}
+        };
+
