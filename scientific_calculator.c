8dbf3e18e740a246a4ae41c706fedf714bae317c
SamraAzizi <fahimaazizi369@gmail.com>
2024-03-17 22:06:50 +0500

    my commit

--- scientific_calculator.c
+++ scientific_calculator.c
@@ -1,73 +1,63 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
+#include <gtk/gtk.h>
 
-double add(double a, double b) {
-    return a + b;
-}
+double num1 = 0, num2 = 0, result = 0;
+char operator = '\0';
 
-double subtract(double a, double b) {
-    return a - b;
-}
-
-double multiply(double a, double b) {
-    return a * b;
-}
-
-double divide(double a, double b) {
-    if (b != 0)
-        return a / b;
-    else {
-        printf("Error: Division by zero\n");
-        exit(1);
-    }
-}
-
-double power(double base, double exponent) {
-    return pow(base, exponent);
-}
-
-int main() {
-    int choice;
-    double num1, num2, result;
-
-    printf("Simple Scientific Calculator\n");
-    printf("----------------------------\n");
-    printf("1. Addition\n");
-    printf("2. Subtraction\n");
-    printf("3. Multiplication\n");
-    printf("4. Division\n");
-    printf("5. Power\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-
-    printf("Enter two numbers: ");
-    scanf("%lf %lf", &num1, &num2);
-
-    switch (choice) {
-        case 1:
-            result = add(num1, num2);
-            printf("Result: %.2lf\n", result);
+void calculate() {
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
             break;
-        case 2:
-            result = subtract(num1, num2);
-            printf("Result: %.2lf\n", result);
+        case '-':
+            result = num1 - num2;
             break;
-        case 3:
-            result = multiply(num1, num2);
-            printf("Result: %.2lf\n", result);
+        case '*':
+            result = num1 * num2;
             break;
-        case 4:
-            result = divide(num1, num2);
-            printf("Result: %.2lf\n", result);
+        case '/':
+            if (num2 != 0)
+                result = num1 / num2;
+            else
+                g_print("Error: Division by zero\n");
             break;
-        case 5:
-            result = power(num1, num2);
-            printf("Result: %.2lf\n", result);
+        case '^':
+            result = pow(num1, num2);
             break;
         default:
-            printf("Invalid choice\n");
+            break;
+    }
+}
+
+void clear() {
+    num1 = 0;
+    num2 = 0;
+    result = 0;
+    operator = '\0';
+}
+
+void button_clicked(GtkWidget *widget, gpointer data) {
+    char *label = gtk_button_get_label(GTK_BUTTON(widget));
+    char *display_text = gtk_label_get_text(GTK_LABEL(data));
+    char buffer[32];
+
+    if (label[0] >= '0' && label[0] <= '9') {
+        strcat(display_text, label);
+    } else if (label[0] == 'C') {
+        strcpy(display_text, "");
+        clear();
+    } else if (label[0] == '=') {
+        num2 = atof(display_text);
+        calculate();
+        sprintf(buffer, "%.2f", result);
+        strcpy(display_text, buffer);
+        clear();
+    } else {
+        num1 = atof(display_text);
+        operator = label[0];
+        strcpy(display_text, "");
     }
 
-    return 0;
+    gtk_label_set_text(GTK_LABEL(data), display_text);
 }
+
+
